1e890cc23a1f09d4c492aaa660463b59a09cbd38
awang23 <awang23@stanford.edu>
2021-06-29 22:18:57 -0500

    connect billing form

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const config = {
+    STRIPE_KEY: "pk_test_51J7UF5Gyx3zC5PuANJkwwjBRCVqgGJH8VXGyelcLLSXMkao4KHmscztZBiTEt3nMBPBXfE3E2WRCA9ArmDAsb6dP00PlVGNo6B",
     MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-1",
